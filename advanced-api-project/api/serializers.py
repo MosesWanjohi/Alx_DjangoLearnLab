5c9a2714a7d01c0a4619b8505f5b595b37b62bd9
Moses Muturi <wanjohimuturi.tech@gmail.com>
2024-09-08 13:04:29 +0300

    Correctly implimented nested BookSerializer inside the AuthorSerializer

--- advanced-api-project/api/serializers.py
+++ advanced-api-project/api/serializers.py
@@ -8,18 +8,19 @@ class BookSerializer(serializers.ModelSerializer):
     publication_year = datetime.now()
     class Meta:
         model = Book
-        fields = ['title', 'publication_year', 'author'] #fields = '__all__'
+        fields = ['title', 'publication_year', 'author'] # or fields = '__all__'
+        
+        #Adding custom validation to the BookSerializer to ensure the publication_year is not in the future.
         for publication_year in fields:
             if publication_year > 'publication_year':
                 raise serializers.ValidationError('Publication year cannot be in the future')
-            
-
-
-
 
 
 #Creating AuthorSerializer
 class AuthorSerializer(serializers.ModelSerializer):
+    
+    #A nested BookSerializer to serialize the related books dynamically.
+    books = BookSerializer(many = True, read_only = True)
     class Meta:
         model = Author
-        fields = ['name', 'BookSerializer(serializers.ModelSerializer)']
+        fields = ['name', 'books']
