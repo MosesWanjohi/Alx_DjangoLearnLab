72cf5223379a40afd9f16db8e0acb2320f37d8f4
Moses Muturi <wanjohimuturi.tech@gmail.com>
2024-09-08 12:06:16 +0300

    modified files in the app

--- advanced-api-project/advanced_api_project/api/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from .models import Book, Author
-from .serializers import BookSerializer, AuthorSerializer
-
-
-# Create your views here.
-class BookView(ApiView):
-    
