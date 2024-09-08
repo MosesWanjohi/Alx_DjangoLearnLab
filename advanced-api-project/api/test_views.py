c63246359e40da03199a3180a8441bea8b844e0c
Moses Muturi <wanjohimuturi.tech@gmail.com>
2024-09-08 18:57:17 +0300

    Installed Token

--- advanced-api-project/api/test_views.py
+++ advanced-api-project/api/test_views.py
@@ -2,13 +2,19 @@ from rest_framework.test import APITestCase
 from django.urls import reverse
 from rest_framework import status
 from .models import Book, Author
+from django.contrib.auth.models import User
+from rest_framework.authentication import TokenAuthentication
+
 
 #Testing CRUD operations for Book model endpoints
 
 class BookListViewTestCase(APITestCase):
     def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+        self.token = TokenAuthentication.objects.create(user=self.user)
+        self.client.login(username='testuser', password='testpassword')
         self.url = reverse('book-list')
-        #Creating a book 
+       
         self.author = Author.objects.create(name='MC Kogi')
         self.book = Book.objects.create(
             title='Take That Shot',
@@ -23,6 +29,9 @@ class BookListViewTestCase(APITestCase):
     
 class BookCreateViewTestCase(APITestCase):
     def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+        self.token = TokenAuthentication.objects.create(user=self.user)
+        self.client.login(username='testuser', password='testpassword')
         self.url = reverse('book-list')
         self.author = Author.objects.create(name='MC Kogi')
         self.book_data = {
@@ -33,13 +42,17 @@ class BookCreateViewTestCase(APITestCase):
 
     def test_book_create_view(self):
         response = self.client.post(self.url, self.book_data)
-
         self.assertEqual(response.status_code, 201)
         self.assertEqual(Book.objects.count(), '1') # 1 (MC Kogi)
         self.assertEqual(Book.objects.get().title, 'Take That Shot')
 
 class BookRetrieveViewTestCase(APITestCase):
     def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+        self.token = TokenAuthentication.objects.create(user=self.user)
+        self.client.login(username='testuser', password='testpassword')
+
+        self.author = Author.objects.create(name='MC Kogi')
         self.book = Book.objects.create(
             title='Take That Shot',
             publication_year=2023,
@@ -54,5 +67,49 @@ class BookRetrieveViewTestCase(APITestCase):
         self.assertEqual(response.data['title'], 'Take That Shot')
         self.assertEqual(response.data['publication_year'], 2023)
 
-#Running tests with python manage.py test
+
+class BookUpdateViewTestCase(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+        self.token = TokenAuthentication.objects.create(user=self.user)
+        self.client.login(username='testuser', password='testpassword')
+        self.author = Author.objects.create(name='MC Kogi')
+        self.book = Book.objects.create(
+            title='Take That Shot',
+            publication_year=2023,
+            author=self.author
+        )
+        self.url = reverse('book-detail', args=[self.book.id])
+        self.book_data = {
+            'title': 'Take That Shot',
+            'publication_year': 2023,
+            'author': self.author.id
+        }
+
+    def test_book_update_view(self):
+        response = self.client.put(self.url, self.book_data)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(Book.objects.get().title, 'Take That Shot')
+
+    class BookDeleteViewTestCase(APITestCase):
+        def setUp(self):
+            self.user = User.objects.create_user(username='testuser', password='testpassword')
+            self.token = TokenAuthentication.objects.create(user=self.user)
+            self.client.login(username='testuser', password='testpassword')
+            self.author = Author.objects.create(name='MC Kogi')
+
+            self.book = Book.objects.create(
+                title='Take That Shot',
+                publication_year=2023,
+                author=self.author
+            )
+            self.url = reverse('book-detail', args=[self.book.id])
+
+        def test_book_delete_view(self):
+            response = self.client.delete(self.url)
+
+            self.assertEqual(response.status_code, 204)
+            self.assertEqual(Book.objects.count(), 0)  
+
 
