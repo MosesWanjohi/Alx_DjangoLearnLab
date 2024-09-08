881e6902440efc561e19569ed7c1bd9e122c71af
Moses Muturi <wanjohimuturi.tech@gmail.com>
2024-09-08 15:26:52 +0300

    Implemented custom validation and permissions in Create and Update Views

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,7 +1,10 @@
+
 from rest_framework import mixins, generics
 from .serializers import BookSerializer, AuthorSerializer
-from django.contrib.auth.models import Book, Author
-
+from .models import Book
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly, IsAdminUser
+from rest_framework import serializers
 class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class =BookSerializer
@@ -12,7 +15,7 @@ class BookListView(generics.ListAPIView):
 
 #Creating a BookDetailView for retrieving a single book by ID
 class BookDetailView(generics.DetailAPIView):
-    queryset = Book.objects.filter('{<pk>}: {<pk>}')   
+    queryset = Book.objects.all()
     serializer_class =BookSerializer
 
     def get(self, request, *args, **kwargs):
@@ -21,20 +24,31 @@ class BookDetailView(generics.DetailAPIView):
 
 #Creating a BookCreateView for adding a new book
 class BookCreateView(generics.CreateAPIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated, IsAuthenticatedOrReadOnly, IsAdminUser]
     queryset = Book.objects.all()   
-    serializer_class =BookSerializer
+    serializer_class = BookSerializer
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
+    def perform_create(self, serializer):
+        title = serializer.validate_data.get('title')
+        if Book.objects.filter(title=title).exists():
+            raise serializers.ValidationError('Book with {self.title} already exists')
+        else:
+            serializers.save()
 
 #Creating a BookUpdateView for modifying an existing book
 class BookUpdateView(generics.UpdateAPIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated, IsAuthenticatedOrReadOnly, IsAdminUser]
     queryset = Book.objects.all()   
     serializer_class =BookSerializer
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
+    def perform_update(self, data, serializer):
+        if len (data['title']) > 50:
+            raise serializers.ValidationError('Title is too long')
+        else:
+            serializer.save()
+
     
 
 #Creating a BookDeleteView for removing an existing book
@@ -42,5 +56,5 @@ class BookDeleteView(generics.DestroyAPIView):
     queryset = Book.objects.all()   
     serializer_class =BookSerializer
 
-    def post(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
+    def perform_destroy(self, instance):
+        instance.delete()
