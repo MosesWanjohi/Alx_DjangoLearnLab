b2b66632c1bf4847f829b6f941b8fe1ef83b7a88
Moses Muturi <wanjohimuturi.tech@gmail.com>
2024-09-08 11:47:55 +0300

    created BookSerializer and AuthorSerializer

--- advanced-api-project/advanced_api_project/api/views.py
+++ advanced-api-project/advanced_api_project/api/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Book, Author
+from .serializers import BookSerializer, AuthorSerializer
+
 
 # Create your views here.
+class BookView(ApiView):
+    
