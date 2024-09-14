28989446c16917164d48f2bb179e5efb2919d0f9
Moses Muturi <wanjohimuturi.tech@gmail.com>
2024-09-15 02:32:30 +0300

    modified project structure

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,8 +1,8 @@
-
 from django import forms
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from .models import Profile
+from taggit.forms import TagWidget
+
 
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -11,12 +11,44 @@ class CustomUserCreationForm(UserCreationForm):
         model = User
         fields = ("username", "email", "password1", "password2")
 
-class ProfileUpdateForm(forms.ModelForm):
-    class Meta:
-        model = Profile
-        fields = ['bio', 'image']
+    def save(self, commit=True):
+        user = super(CustomUserCreationForm, self).save(commit=False)
+        user.email = self.cleaned_data["email"]
+        if commit:
+            user.save()
+        return user
+    
+from .models import Post
 
-class UserUpdateForm(forms.ModelForm):
+class PostForm(forms.Form):
     class Meta:
-        model = User
-        fields = ['username', 'email']
+        model = Post
+        fields = ("title", "content", "image", "tags")
+        
+        widgets = {
+            'tags': TagWidget()
+        }
+      
+    def clean_content(self):
+        content = self.cleaned_data.get("content")
+        if not content:
+            raise forms.ValidationError("Content is required")
+        return content
+
+
+
+
+
+from .models import Comment
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ("content",)
+
+    def clean_content(self):
+        content = self.cleaned_data.get("content")
+        if not content or len(content) < 5:
+            raise forms.ValidationError("Comment must be at least 5 characters long")
+        return content
+    
