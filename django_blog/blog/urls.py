f621b6c247df2960466ecfd72f6590a4ce431ab3
Moses Muturi <wanjohimuturi.tech@gmail.com>
2024-09-14 08:12:30 +0300

    defined urls for the various views

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,5 +1,14 @@
 
+from django.urls import path
+from .import views
 
-from django.contrib import admin
-from django.urls import path, include
+
+urlpatterns = [
+    path('register/', views.register, name='register'),  # Registration URL
+    path('login/', views.user_login, name='login'),      # Login URL
+    path('logout/', views.user_logout, name='logout'),   # Logout URL
+    path('profile/', views.profile, name='profile'),     # Profile URL
+]
+
+
 
