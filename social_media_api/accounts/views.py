25a5011cd2f24909dc1bcbf2abc0b8f033d64984
Moses Muturi <wanjohimuturi.tech@gmail.com>
2024-09-22 14:13:08 +0300

    Correctly referenced the CustomUser model

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -85,12 +85,12 @@ class FollowUserView(generics.GenericAPIView):
 
     #Following user
     def post(self, request, user_id):
-        user_to_follow = get_object_or_404(User.objects.all(), id=user_id)
+        user_to_follow = get_object_or_404(CustomUser.objects.all(), id=user_id)
         request.user.following.add(user_to_follow)
         return Response({'message': f'You are now following {user_to_follow.username}'}, status=status.HTTP_200_OK)
 
 #Unfollowing user
     def destroy(self, request, user_id):
-        user_to_unfollow = get_object_or_404(User.objects.all(), id=user_id)
+        user_to_unfollow = get_object_or_404(CustomUser.objects.all(), id=user_id)
         request.user.following.remove(user_to_unfollow)
         return Response({'message': f'You have unfollowed {user_to_unfollow.username}'}, status=status.HTTP_200_OK)
