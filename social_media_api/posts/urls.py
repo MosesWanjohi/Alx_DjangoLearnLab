685c977356775b27478da8ac4b21faa306ae12ca
Moses Muturi <wanjohimuturi.tech@gmail.com>
2024-09-22 15:01:50 +0300

    rectified typo for feed route in posts/urls.py

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -5,7 +5,7 @@ from .views import PostViewSet, CommentViewSet, FeedViewSet
 router = DefaultRouter()
 router.register(r'posts', PostViewSet, basename='posts')
 router.register(r'comments', CommentViewSet, basename='comments')
-router.register(r'feed', FeedViewSet, basename='feed')
+router.register(r'feed/', FeedViewSet, basename='feed')
 
 urlpatterns = [
     path("", include(router.urls)),
